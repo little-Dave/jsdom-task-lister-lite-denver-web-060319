f8e09b4eadd2dab00f7e2c4d00bd1302a30fb10f
little-Dave <17536424+little-Dave@users.noreply.github.com>
2019-07-22 16:08:39 -0600

    Add items to ToDo list

--- src/index.js
+++ src/index.js
@@ -1,3 +1,25 @@
 document.addEventListener("DOMContentLoaded", () => {
   // your code here
+  const list = document.getElementById('list');
+  const form = document.getElementById('create-task-form');
+  const newTask = document.getElementById('new-task-description')
+
+  form.addEventListener('submit', function(event) {
+    event.preventDefault();
+    addTodo();
+    newTask.value = '';
+  })
+
+  function addTodo() {
+    let item = document.createElement('li');
+    item.innerText = newTask.value
+    list.appendChild(item);
+  }
+
+  // function addDeleteButton () {
+  //   deleteItem = document.createElement('button');
+  //   deleteItem.innerText = 'x';
+  //   deleteItem.addEventListener('click', removeItemFromList);
+  // }
+
 });
